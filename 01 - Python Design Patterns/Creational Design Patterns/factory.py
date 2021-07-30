8537758b26ea44bed2a4ca9a37d9bc3c9536c1bb
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2021-07-29 23:12:42 -0500

    refactor

---
+++ 01 - Python Design Patterns/Creational Design Patterns/factory.py
@@ -0,0 +1,31 @@
+# Scenario: Pet shop used to sell only dogs, but now needs to be able to sell cats too.
+
+class Dog:
+
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Woof!"
+
+class Cat:
+
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Meow!"
+
+def get_pet(pet="dog"):
+
+    """The factory method"""
+    
+    pets = dict(dog=Dog(name="Hope"), cat=Cat("Peace"))
+
+    return pets[pet]
+
+d = get_pet(pet="dog")
+print(d.speak())
+
+c = get_pet(pet="cat")
+print(c.speak())
