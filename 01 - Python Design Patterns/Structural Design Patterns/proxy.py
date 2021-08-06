b6811cb75a802a12a09792f438bd44d570b3af70
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2021-08-06 18:41:34 -0500

    commit

--- 01 - Python Design Patterns/Structural Design Patterns/proxy.py
+++ 01 - Python Design Patterns/Structural Design Patterns/proxy.py
@@ -1,44 +1,50 @@
 import time
 
+
+# Scenario: Only a fixed number of Producer artists can exist at a given time. The proxy (the artist)
+# checks to see if the producer is available and is responsible for creating the producer objects.f
+
 class Producer:
-	"""Define the 'resource-intensive' object to instantiate!"""
-	def produce(self):
-		print("Producer is working hard!")
+    """Define the 'resource-intensive' object to instantiate!"""
 
-	def meet(self):
-		print("Producer has time to meet you now!")
+    def produce(self):
+        print("Producer is working hard!")
 
+    def meet(self):
+        print("Producer has time to meet you now!")
+
+
+# A proxy object can be treated as a middleman to be used until the target object can be instantiated.
 class Proxy:
-	""""Define the 'relatively less resource-intensive' proxy to instantiate as a middleman"""
-	def __init__(self):  
-		self.occupied = 'No'
-		self.producer = None
+    """"Define the 'relatively less resource-intensive' proxy to instantiate as a middleman"""
 
-	def produce(self):
-		"""Check if Producer is available"""
-		print("Artist checking if Producer is available ...")
+    def __init__(self):
+        self.occupied = 'No'
+        self.producer = None
 
-		if self.occupied == 'No':
-			#If the producer is available, create a producer object!
-			
+    def produce(self):
+        """Check if Producer is available"""
+        print("Artist checking if Producer is available ...")
 
-			#Make the prodcuer meet the guest!
-			
-			
-		else:
-			#Otherwise, don't instantiate a producer 
-			time.sleep(2)
-			print("Producer is busy!")
+        if self.occupied == 'No':
+            # If the producer is available, create a producer object!
+            self.producer = Producer()
 
-#Instantiate a Proxy
+            # Make the prodcuer meet the guest!
+            self.producer.meet()
 
+        else:
+            # Otherwise, don't instantiate a producer
+            print("Producer is busy!")
 
-#Make the proxy: Artist produce until Producer is available
+# Instantiate a Proxy
+p = Proxy()
 
+# Make the proxy: Artist produce until Producer is available
+p.produce()
 
-#Change the state to 'occupied'
+# Change the state to 'occupied'
+p.occupied = "Yes"
 
-
-#Make the Producer produce
-
-
+# Make the Producer produce
+p.produce()
